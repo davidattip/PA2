4c4701b6adc5d1bda4671c993bd6167bd928ee6d
davidattip <138945499+davidattip@users.noreply.github.com>
2024-04-06 19:25:40 +0200

    1 er commit
    il faut je commit pour avancer

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -9,38 +9,34 @@ const Navbar = () => {
           <div className="flex space-x-7">
             <div>
               {/* Website Logo */}
-              <a href="#" className="flex items-center py-4 px-2">
-                <Image src="/logo_pcs.png" alt="Logo" width={90} height={45} />
-                <span className="font-semibold text-gray-500 text-lg">airbnb</span>
-              </a>
+              <Link href="/" passHref>
+                <a className="flex items-center py-4 px-2">
+                  <Image src="/logo_pcs.png" alt="Logo" width={90} height={45} />
+                  <span className="font-semibold text-gray-500 text-lg">PCS</span>
+                </a>
+              </Link>
             </div>
             {/* Primary Navbar items */}
             <div className="hidden md:flex items-center space-x-1">
-              <Link href="/logements"><a className="py-4 px-2 text-gray-500 font-semibold hover:text-red-500 transition duration-300">Logements</a></Link>
-              <Link href="/experiences"><a className="py-4 px-2 text-gray-500 font-semibold hover:text-red-500 transition duration-300">Expériences</a></Link>
-              <Link href="/online-experiences"><a className="py-4 px-2 text-gray-500 font-semibold hover:text-red-500 transition duration-300">Expériences en ligne</a></Link>
+              <Link href="/logements" passHref>
+                <a className="py-4 px-2 text-gray-500 font-semibold hover:text-red-500 transition duration-300">Logements</a>
+              </Link>
+              <Link href="/experiences" passHref>
+                <a className="py-4 px-2 text-gray-500 font-semibold hover:text-red-500 transition duration-300">Expériences</a>
+              </Link>
+              <Link href="/online-experiences" passHref>
+                <a className="py-4 px-2 text-gray-500 font-semibold hover:text-red-500 transition duration-300">Expériences en ligne</a>
+              </Link>
             </div>
           </div>
           {/* Secondary Navbar items */}
           <div className="hidden md:flex items-center space-x-3 ">
-            <Link href="/host"><a className="py-2 px-2 font-medium text-gray-500 rounded hover:bg-red-500 hover:text-white transition duration-300">Mettre mon logement sur Airbnb</a></Link>
+            <Link href="/host" passHref>
+              <a className="py-2 px-2 font-medium text-gray-500 rounded hover:bg-red-500 hover:text-white transition duration-300">Mettre mon logement sur PCS</a>
+            </Link>
           </div>
           {/* Mobile menu button */}
-          <div className="md:hidden flex items-center">
-            <button className="outline-none mobile-menu-button">
-              <svg className=" w-6 h-6 text-gray-500 hover:text-red-500 "
-                x-show="!showMenu"
-                fill="none"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path d="M4 6h16M4 12h16m-7 6h7"></path>
-              </svg>
-            </button>
-          </div>
+          {/* Your existing button code */}
         </div>
       </div>
       {/* mobile menu */}
