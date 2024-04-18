84542fbd397e890e07196c1b3d8ae588482907d0
DrTenma <113189733+Drtenma34@users.noreply.github.com>
2024-04-18 14:26:42 +0200

    Débuggage du backend pr run
    Route renter ajusté

--- backend/routes/renterRoutes.js
+++ backend/routes/renterRoutes.js
@@ -1,9 +1,24 @@
 const express = require('express');
+const { authenticateJWT } = require('../middleware/authenticateToken');
 const { isRenter } = require('../middleware/roleMiddleware');
 const router = express.Router();
 
-router.get('/profile', isRenter, (req, res) => {
+// Utilisez authenticateJWT pour vérifier l'authenticité du token
+// puis isRenter pour confirmer que l'utilisateur est un renter
+router.get('/profile', authenticateJWT, isRenter, (req, res) => {
     // Logique du profil rentier
+    // Supposons que vous voulez renvoyer des informations de profil de base
+    if (!req.user) {
+        return res.status(404).send("Profil non trouvé.");
+    }
+    // Accéder à des données utilisateur plus détaillées peut nécessiter une requête à la DB
+    // Ici, nous retournons simplement des informations simplifiées pour l'exemple
+    res.json({
+        userId: req.user.userId,
+        email: req.user.email,
+        role: req.user.user_type,
+        message: "Profil du rentier accessible."
+    });
 });
 
 module.exports = router;
