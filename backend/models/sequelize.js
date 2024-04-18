6ce0d503c29f333c9527d71cd5468ee370236d2e
DrTenma <113189733+Drtenma34@users.noreply.github.com>
2024-04-18 16:02:07 +0200

    .env modifié pour MAMP

--- backend/models/sequelize.js
+++ backend/models/sequelize.js
@@ -1,9 +1,16 @@
+require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-// Mes informations de connexion MySQL
-const sequelize = new Sequelize('pcs_db', 'root', 'pcs_beauprojet', {
-    host: '127.0.0.1',
-    dialect: 'mysql' // Je m'assure que le dialecte est défini sur 'mysql'
-});
+// Utilisation des variables d'environnement pour la configuration
+const sequelize = new Sequelize(
+    process.env.DB_NAME,     // Nom de la base de données
+    process.env.DB_USER,     // Utilisateur
+    process.env.DB_PASS,     // Mot de passe
+    {
+        host: process.env.DB_HOST, // Hôte
+        port: process.env.DB_PORT, // Port ajouté ici
+        dialect: 'mysql'          // Dialecte
+    }
+);
 
 module.exports = sequelize;
