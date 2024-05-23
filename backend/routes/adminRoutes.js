23d3122d305879b6a835b7b4bba892281a8a2db8
DrTenma <113189733+Drtenma34@users.noreply.github.com>
2024-05-23 23:11:12 +0200

    2 routes api pr obtenir / modifer les détails d'un utilisateur par ID

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -5,7 +5,12 @@ const { isAdmin } = require('../middleware/roleMiddleware');
 
 const router = express.Router();
 
-// S'assurer que la requête est d'abord passée par authenticateJWT puis par isAdmin
+// S'assurer que la requête est d'abord passée par authenticateJWT puis par isAdmin.
+
+// 3 routes dans le fichier
+
+// Route pour afficher la liste des utilisateurs avec pagination
+
 router.get('/backoffice/users', authenticateJWT, isAdmin, async (req, res) => {
 
     // ajouter des paramètres de requête pour la page et la limite, avec des valeurs par défaut si elles ne sont pas fournies
@@ -38,4 +43,64 @@ router.get('/backoffice/users', authenticateJWT, isAdmin, async (req, res) => {
     }
 });
 
+// Route pour obtenir les détails d'un utilisateur par ID
+router.get('/backoffice/users/:id', authenticateJWT, isAdmin, async (req, res) => {
+    const userId = req.params.id;
+
+    try {
+        const user = await User.findOne({
+            attributes: ['id', 'email', 'first_name', 'last_name', 'user_type'],
+            where: { id: userId }
+        });
+
+        if (!user) {
+            return res.status(404).json({ message: "Utilisateur non trouvé." });
+        }
+
+        res.status(200).json(user);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Erreur interne du serveur." });
+    }
+});
+
+// Route pour modifier les informations d'un utilisateur par ID
+
+
+router.put('/backoffice/users/:id', authenticateJWT, isAdmin, async (req, res) => {
+    const userId = req.params.id;
+    const { first_name, last_name, email, user_type, password } = req.body;
+
+    try {
+        const user = await User.findOne({ where: { id: userId } });
+
+        if (!user) {
+            return res.status(404).json({ message: "Utilisateur non trouvé." });
+        }
+
+        if (first_name) user.first_name = first_name;
+        if (last_name) user.last_name = last_name;
+        if (email) user.email = email;
+        if (user_type) user.user_type = user_type;
+        if (password) user.password_hash = await bcrypt.hash(password, 10);
+
+        await user.save();
+
+        res.status(200).json({
+            message: "Utilisateur mis à jour avec succès.",
+            user: {
+                id: user.id,
+                first_name: user.first_name,
+                last_name: user.last_name,
+                email: user.email,
+                user_type: user.user_type
+            }
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Erreur interne du serveur." });
+    }
+});
+
 module.exports = router;
+
