9e3391a7c7b1f976df349468d483c1d319bda1ae
DrTenma <113189733+Drtenma34@users.noreply.github.com>
2024-07-16 16:41:56 +0200

    ServiceType ajouté

---
+++ backend/models/ServiceType.js
@@ -0,0 +1,34 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('./sequelize');
+
+const ServiceType = sequelize.define('ServiceType', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    description: {
+        type: DataTypes.TEXT,
+        allowNull: true
+    },
+    price: {
+        type: DataTypes.DECIMAL,
+        allowNull: false
+    },
+    createdAt: {
+        type: DataTypes.DATE,
+        allowNull: true
+    },
+    updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: true
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = ServiceType;
