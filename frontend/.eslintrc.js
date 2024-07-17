f3376626539657e5a98fcab13088d39336223b81
DrTenma <113189733+Drtenma34@users.noreply.github.com>
2024-07-18 01:51:07 +0200

    Eslint debug attempt

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    extends: 'next',
+    rules: {
+        '@next/next/no-img-element': 'off',
+        'react/no-unescaped-entities': 'off',
+        'react-hooks/exhaustive-deps': 'off',
+        '@next/next/no-html-link-for-pages': 'off',
+    },
+};
